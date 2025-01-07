92f930d2500cfece8c66b7530dd7fdc26d2bdc7f
wangyaqi <wyq522208@mail.ustc.edu.cn>
2025-01-07 07:08:20 +0000

    feat:添加文本分类微调模型测试

--- finetune.py
+++ finetune.py
@@ -42,13 +42,16 @@ class SpamDataset(Dataset):
             torch.tensor(self.data["Label"][index])
         )
 
+train_dataset_max_length = None
+
 def get_dataloader():
     train_dataset = SpamDataset(
         "./data/classify_finetune/train.csv",
         max_length=None,
         tokenizer=tokenizer
     )
-
+    global train_dataset_max_length
+    train_dataset_max_length = train_dataset.max_length
     val_dataset = SpamDataset(
         "./data/classify_finetune/validation.csv",
         max_length=train_dataset.max_length,
@@ -230,3 +233,48 @@ train_losses, val_losses, train_accs, val_accs, examples_seen = train_classifier
 end_time = time.time()
 execution_time_minutes = (end_time - start_time) / 60
 print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+
+def classify_review(text, model, tokenizer, device, max_length=None, pad_token_id=50256):
+    model.eval()
+
+    # 准备模型的输入
+    input_ids = tokenizer.encode(text)
+    supported_context_length = model.pos_emb.weight.shape[1]
+
+    # 如果序列太长则截断
+    input_ids = input_ids[:min(max_length, supported_context_length)]
+
+    # 将序列填充到最长序列
+    input_ids += [pad_token_id] * (max_length - len(input_ids))
+    input_tensor = torch.tensor(input_ids, device=device).unsqueeze(0) # 添加批次维度
+
+    # 模型推理
+    with torch.no_grad():
+        logits = model(input_tensor)[:, -1, :]  # 最后一个输出 token 的 Logits
+    predicted_label = torch.argmax(logits, dim=-1).item()
+
+    # 返回分类结果
+    return "spam" if predicted_label == 1 else "not spam"
+
+text_1 = (
+    "You are a winner you have been specially"
+    " selected to receive $1000 cash or a $2000 award."
+)
+
+print(classify_review(
+    text_1, model, tokenizer, device, max_length=train_dataset_max_length
+))
+
+text_2 = (
+    "Hey, just wanted to check if we're still on"
+    " for dinner tonight? Let me know!"
+)
+
+print(classify_review(
+    text_2, model, tokenizer, device, max_length=train_dataset_max_length
+))
+# 保存模型
+torch.save(model.state_dict(), "weights/classify-finetune/classifier.pth")
+
+# model_state_dict = torch.load("review_classifier.pth")
+# model.load_state_dict(model_state_dict)
